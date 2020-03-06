51692223285eb1bff5d77b3db233e56c368eab03
Alex <alex.abruzzese96@gmail.com>
2020-03-06 14:29:58 +0100

    red return the sum with one character

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -8,11 +8,17 @@ import static org.junit.Assert.assertThat;
 
 public class UnitTest {
     private static final StringCalculator stringcalculator = new StringCalculator();
-    public String number = "";
+    public String number = "3";
 
     @Test
     public void should_return_zero_if_string_is_empty()
     {
         assertThat(stringcalculator.add(number), equalTo("0"));
+    }
+
+    @Test
+    public void should_return_sum_of_the_string_with_one_paramater()
+    {
+        assertThat(stringcalculator.add(number), equalTo(number));
     }
 }
