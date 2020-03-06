2227a218e70463c05aa08cb845f6e416d4cd7c36
Alex <alex.abruzzese96@gmail.com>
2020-03-06 14:40:53 +0100

    red return the sum with two params without dot

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertThat;
 public class UnitTest {
     private static final StringCalculator stringcalculator = new StringCalculator();
     public String number = "3";
+    public String number_to_add = "1";
+    public String addition = number + "," + number_to_add;
 
     @Test
     public void should_return_zero_if_string_is_empty()
@@ -20,5 +22,11 @@ public class UnitTest {
     public void should_return_sum_of_the_string_with_one_paramater()
     {
         assertThat(stringcalculator.add(number), equalTo(number));
+    }
+
+    @Test
+    public void should_return_sum_of_the_string_with_two_paramaters_and_without_dot()
+    {
+        assertThat(stringcalculator.add(addition), equalTo(Integer.toString(Integer.parseInt(number) + Integer.parseInt(number_to_add))));
     }
 }
