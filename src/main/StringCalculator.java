517db3dbdf07d4d03d4f0a04592d2579a28c69cb
Alex <alex.abruzzese96@gmail.com>
2020-03-06 14:27:39 +0100

    refractor return 0 char if input string is empty

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -3,6 +3,7 @@ package main;
 public class StringCalculator {
     public String add(String number)
     {
+        //condition who check the length of the input string
         if (number.length() == 0)
         {
             return "0";
